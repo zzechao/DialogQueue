ea46d8554a20afa64accea45ca1e6684d35daeb3
zhouz <zhouzechao10@163.com>
2024-01-23 16:36:50 +0800

    1、发布尝试

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -2,15 +2,15 @@ plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
 }
-
+apply {
+    from("${rootProject.projectDir}/publish-mavencentral.gradle")
+}
 android {
     namespace = "com.zhouz.dialogqueue"
     compileSdk = 31
 
     defaultConfig {
         minSdk = 15
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
     }
 
@@ -18,8 +18,7 @@ android {
         release {
             isMinifyEnabled = false
             proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
+                getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro"
             )
         }
     }
@@ -35,10 +34,5 @@ android {
 dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.10.0")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
 }
